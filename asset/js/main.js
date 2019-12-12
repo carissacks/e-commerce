0499f2a04d0558145155f34644390d3a39bb6469
carissacks <carissa.cks@gmail.com>
2019-12-12 20:59:04 +0700

    cart revised

--- asset/js/main.js
+++ asset/js/main.js
@@ -141,13 +141,13 @@
 
     /*==================================================================
     [ Cart ]*/
-    // $('.js-show-cart').on('click',function(){
-    //     $('.js-panel-cart').addClass('show-header-cart');
-    // });
+    $('.js-show-cart').on('click',function(){
+        $('.js-panel-cart').addClass('show-header-cart');
+    });
 
-    // $('.js-hide-cart').on('click',function(){
-    //     $('.js-panel-cart').removeClass('show-header-cart');
-    // });
+    $('.js-hide-cart').on('click',function(){
+        $('.js-panel-cart').removeClass('show-header-cart');
+    });
 
     /*==================================================================
     [ Cart ]*/
